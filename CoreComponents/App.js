3a4f1d7101b2f212f00072512e5908b29ee2dc02
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-11-30 07:56:39 +0800

    Update App.js

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Login from "./screens/Login";
 import Dashboard from './screens/Dashboard';
+import Register from "./screens/Register";
 
 const Stack = createNativeStackNavigator()
 
@@ -9,11 +11,14 @@ export default function App() {
 
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login" screenOptions={{
-          headerShown: false
+        <Stack.Navigator 
+			initialRouteName="Login" 
+			screenOptions={{
+            	headerShown: false
         }}>
             <Stack.Screen name="Login" component={Login}/>
             <Stack.Screen name="Dashboard" component={Dashboard}/>
+            <Stack.Screen name="Register" component={Register}/>
         </Stack.Navigator>
     </NavigationContainer>
   );
