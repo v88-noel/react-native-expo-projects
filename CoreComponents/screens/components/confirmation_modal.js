bca79897675843be04e1180d29d1bd63a51d9dfd
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-11-29 22:10:33 +0800

    Update confirmation_modal.js

--- CoreComponents/screens/components/confirmation_modal.js
+++ CoreComponents/screens/components/confirmation_modal.js
@@ -1,36 +1,45 @@
 import Modal from "react-native-modal";
-import { Pressable, StyleSheet, Text, TextInput, View, Dimensions, Image  } from "react-native";
+import { Pressable, StyleSheet, Text, View, Image  } from "react-native";
 
-export default function ConfirmationModal({}) {
-  return (
-    <Modal  isVisible={false} style={styles.modal_container}>
-        <View style={ styles.modal_content }>
-            <Pressable style={styles.close_button}>
-                <Image
-                    source={require("../../assets/action_icons/cancel.png")}
-                    fadeDuration={0}
-                    style={{ width: 24, height: 24 }}
-                />
-            </Pressable>
+export default function ConfirmationModal({is_visible, onReturnConfirmationModalResult, modal_type}) {
 
-            <Text style={styles.modal_title}>Confirm Delete Message</Text>
-            
-            <View style={styles.modal_confirmation_message_container}>
-                <Text style={styles.modal_confirmation_message}>Are you sure you want to remove this message? This action cannot be undone.</Text>
-            </View>
+    const onPressCancelAction = () =>{
+        onReturnConfirmationModalResult(false, modal_type);
+    }
+    
+    const onPressAcceptAction = () =>{
+        onReturnConfirmationModalResult(true, modal_type);
+    }
 
-            <View style={styles.modal_action_container}>
-                <Pressable style={styles.modal_cancel_button}>
-                    <Text style={styles.modal_cancel_button_text}>Cancel</Text>
+    return (
+        <Modal  isVisible={is_visible} style={styles.modal_container}>
+            <View style={ styles.modal_content }>
+                <Pressable style={styles.close_button} onPress={onPressCancelAction}>
+                    <Image
+                        source={require("../../assets/action_icons/cancel.png")}
+                        fadeDuration={0}
+                        style={{ width: 24, height: 24 }}
+                    />
                 </Pressable>
 
-                <Pressable style={styles.modal_accept_button}>
-                    <Text  style={styles.modal_accept_button_text}>Yes, Remove it.</Text>
-                </Pressable>
+                <Text style={styles.modal_title}>Confirm Delete {modal_type.charAt(0).toUpperCase() + modal_type.slice(1)}</Text>
+                
+                <View style={styles.modal_confirmation_message_container}>
+                    <Text style={styles.modal_confirmation_message}>Are you sure you want to remove this {modal_type}? This action cannot be undone.</Text>
+                </View>
+
+                <View style={styles.modal_action_container}>
+                    <Pressable style={styles.modal_cancel_button} onPress={onPressCancelAction}>
+                        <Text style={styles.modal_cancel_button_text}>Cancel</Text>
+                    </Pressable>
+
+                    <Pressable style={styles.modal_accept_button} onPress={onPressAcceptAction}>
+                        <Text  style={styles.modal_accept_button_text}>Yes, remove it.</Text>
+                    </Pressable>
+                </View>
             </View>
-        </View>
-    </Modal>
-  )
+        </Modal>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -71,11 +80,11 @@ const styles = StyleSheet.create({
         paddingBottom: 8
     },
     modal_cancel_button: {
-        marginRight: 8
+        marginRight: 16
     },
     modal_cancel_button_text:{
         color: "rgba(0, 0, 0, 0.60)",
-        fontSize: 14,
+        fontSize: 16,
         fontWeight: "500"
     },
     modal_accept_button: {
@@ -86,5 +95,6 @@ const styles = StyleSheet.create({
     },
     modal_accept_button_text:{
         color: "#fff",
+        fontSize: 16
     }
 })
