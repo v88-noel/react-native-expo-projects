ab827df26316c457a3acfec178220faff5913379
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-12-01 11:26:34 +0800

    Create app_context.js

---
+++ CoreComponents/config/app_context.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from "react";
+import wall_data from "../assets/data.json"; 
+
+const AppContext = React.createContext();
+const AppUpdateContext = React.createContext();
+
+export function useData(){
+    return useContext(AppContext)
+}
+
+export function useUpdateData(){
+    return useContext(AppUpdateContext)
+}
+
+export function AppContextProvider({children}){
+    const [app_data, setAppData] = useState(wall_data);
+    
+    function updateData(new_message_value){
+        
+        const new_message = {
+            "id": app_data.length + 1,
+            "message_content": new_message_value,
+            "comments": []
+        };
+            
+        setAppData(previous_messages => [...previous_messages, new_message]);   
+    }
+
+    return (
+        <AppContext.Provider value={app_data}>
+            <AppUpdateContext.Provider value={updateData}>
+                {children}
+            </AppUpdateContext.Provider>
+        </AppContext.Provider>
+    )
+}
