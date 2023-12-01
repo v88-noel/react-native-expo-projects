4d87e2e40010860578669e8b168f9f6c26c17eda
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-12-01 16:41:28 +0800

    Update message_styles.js

--- CoreComponents/assets/styles/message_styles.js
+++ CoreComponents/assets/styles/message_styles.js
@@ -3,7 +3,9 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     message_container:{
         backgroundColor: "#fff",
-        height: "100%"
+        height: "100%",
+        paddingHorizontal: 16,
+        paddingTop: 48
     }
 });
 
