c90e32c87c704007e1b314af84950f688d9b7a03
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-12-01 11:26:33 +0800

    Update App.js

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,28 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AppContextProvider } from './config/app_context';
+import React from "react";
 
 import Login from "./screens/Login";
 import Dashboard from './screens/Dashboard';
 import Register from "./screens/Register";
 import Message from './screens/Message';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
+export const AppContext = React.createContext();
 
 export default function App() {
 
 	return (
-		<NavigationContainer>
-			<Stack.Navigator 
-			initialRouteName="Login" 
-			screenOptions={{
-					headerShown: false
-			}}>
-				<Stack.Screen name="Login" component={Login}/>
-				<Stack.Screen name="Dashboard" component={Dashboard}/>
-				<Stack.Screen name="Register" component={Register}/>
-				<Stack.Screen name="Message" component={Message}/>
-			</Stack.Navigator>
-		</NavigationContainer>
+		<AppContextProvider>
+			<NavigationContainer>
+				<Stack.Navigator 
+				initialRouteName="Login" 
+				screenOptions={{
+						headerShown: false
+				}}>
+					<Stack.Screen name="Login" component={Login}/>
+					<Stack.Screen name="Dashboard" component={Dashboard}/>
+					<Stack.Screen name="Register" component={Register}/>
+					<Stack.Screen name="Message" component={Message}/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</AppContextProvider>
 	);
 }
 
